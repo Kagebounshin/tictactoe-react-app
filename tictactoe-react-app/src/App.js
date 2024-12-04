4fa9f685d59875e54c13d6f9a8ff6e3945052597
Kristoffer Heiskanen <77610394+Kagebounshin@users.noreply.github.com>
2024-12-04 19:23:12 +0100

    Added base tic tac toe code, and styling

--- tictactoe-react-app/src/App.js
+++ tictactoe-react-app/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function Square({ value, onSquaresClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <button 
+    className="square"
+    onClick={ onSquaresClick }
+    >
+      { value }
+    </button>
+  );
+}
+
+function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else  {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  return (
+    <>
+      <div className="board">
+        <div className="board-row">
+          <Square value={squares[0]} onSquaresClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquaresClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquaresClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquaresClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquaresClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquaresClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquaresClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquaresClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquaresClick={() => handleClick(8)} /> 
+        </div>
+      </div>
+    </>
   );
 }
 
