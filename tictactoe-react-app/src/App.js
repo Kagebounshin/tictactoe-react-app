4dc86ad6e841a3aa8261fb5e7df6765165aec3a2
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2024-12-07 08:16:26 +0100

    function to track game history,

--- tictactoe-react-app/src/App.js
+++ tictactoe-react-app/src/App.js
@@ -12,7 +12,7 @@ function Square({ value, onSquaresClick }) {
   );
 }
 
-function App() {
+function App({xIsNext, squares, onPlay}) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -26,6 +26,7 @@ function App() {
     } else  {
       nextSquares[i] = 'O';
     }
+    onPlay(nextSquares);
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   };
@@ -56,7 +57,7 @@ function App() {
     status = "Winner: " + winner;
   } else {
     status = "Next Player: " + (xIsNext ? "X" : "O"); 
-  }
+  } 
 
   return (
     <>
@@ -80,6 +81,30 @@ function App() {
       </div>
     </>
   );
+}
+
+function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1]; 
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <>
+    <div className="game">
+      <div className="game-board">
+        <App xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{ /*TODO*/ }</ol>
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default App;
