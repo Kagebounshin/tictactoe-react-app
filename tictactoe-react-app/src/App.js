d5ba152bcc6c3eb04dc45a4adb9d4adad8567c9a
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2024-12-07 09:30:14 +0100

    Cleand up code, hover state for button elements

--- tictactoe-react-app/src/App.js
+++ tictactoe-react-app/src/App.js
@@ -11,10 +11,10 @@ function Square({ value, onSquaresClick }) {
   );
 }
 
-function Board({xIsNext, squares, onPlay}) {
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -25,26 +25,6 @@ function Board({xIsNext, squares, onPlay}) {
     }
     onPlay(nextSquares);
   };
-
-  function calculateWinner(squares) {
-    const winOptions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i in winOptions) {
-      const [a,b ,c] = winOptions[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      };
-    };
-    return null;
-  }
 
   const winner = calculateWinner(squares);
   let status;
@@ -79,21 +59,19 @@ function Board({xIsNext, squares, onPlay}) {
 }
 
 function App() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove]; 
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length -1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
@@ -124,6 +102,26 @@ function App() {
     </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const winOptions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i in winOptions) {
+    const [a,b ,c] = winOptions[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    };
+  };
+  return null;
 }
 
 export default App;
